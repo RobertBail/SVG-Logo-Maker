05a0a8a52440170a979816709f4f3c6d93a88f91
Robert Bailey <r.bail@hotmail.com>
2023-11-26 19:34:04 +1030

    logo maker changes

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 - This application allows a web developer to generate a simple svg logo for their project
 - They can choose the shape of the logo, Circle, Square or Triangle and input the color for the shape and logo text
 ## Link to Video
-https://drive.google.com/file/d/1hsOIiQ0cUmHUfAVSmYSf_DNVwkbS4Rlt/view?usp=sharing
+https://drive.google.com/file/d/1XSMJePIQM5EIIvUZDHH0O-aS8xJfQy_K/view?usp=drive_link
--- index.js
+++ index.js
@@ -1,6 +1,50 @@
-const SHAPES = require('./lib/shapes.js');
-//or Shapes?
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Circle = require("./lib/Circle")
+const Square = require("./lib/Square")
+const Triangle = require("./lib/Triangle")
+//run?
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Please enter the logo name, no more than 3 characters.',
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Please enter a color keyword or hexadecimal number for the text.',
+    },
+    {
+      type: 'list',
+      name: 'shapeChoice',
+      message: `Please select logo shape`,
+      choices: ['Circle', 'Square', 'Triangle'],
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Please enter a color keyword or hexadecimal number for the shape color.',
+    },
+  ])
+  .then((answers) => {
 
-const shapes = new SHAPES();
+    if (answers.text.length > 3) {
+        throw new Error('Logo name cannot be more than 3 characters');
+    }
 
-shapes.run();
+    if (answers.shapeChoice == "Circle") {
+        var output = new Circle(answers.text, answers.textColor, answers.shapeColor, answers.shapeChoice)
+    }
+    if (answers.shapeChoice == "Square") {
+        var output = new Square(answers.text, answers.textColor, answers.shapeColor, answers.shapeChoice)
+    }
+    if (answers.shapeChoice == "Triangle") {
+        var output = new Triangle(answers.text, answers.textColor, answers.shapeColor, answers.shapeChoice)
+    }
+  
+    fs.writeFile("Logo.svg", output.render(), (err) => err ? console.log(err) : console.log('Successfully generated logo.svg'));
+
+  })
+  .catch((err) => console.error(err));
